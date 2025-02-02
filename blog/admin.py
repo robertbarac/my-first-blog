47882f44d0463c860e21a20ecc9763408d012a8d
Robert Barrios <ryba-07@hotmail.com>
2025-02-02 00:44:46 -0500

    Creando login y comentarios y sus views

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('author', 'title', 'text')
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'text', 'created_date', 'approved_comment')
+    ordering = ('created_date')
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
